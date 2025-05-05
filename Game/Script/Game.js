406c49f57b16c89eb0674c24a4a2138684520b8c
pupungeggang6 <pupungeggang6@gmail.com>
2025-05-06 00:04:36 +0900

    Change

--- Game/Script/Game.js
+++ Game/Script/Game.js
@@ -7,7 +7,7 @@ class Game {
         this.canvas = document.getElementById('screen')
         this.ctx = this.canvas.getContext('2d')
 
-        window.addEventListener('mouseup', (event) => this.mouseUp(event), false)
+        this.canvas.addEventListener('mouseup', (event) => this.mouseUp(event), false)
         window.addEventListener('keydown', (event) => this.keyDown(event), false)
         window.addEventListener('keyup', (event) => this.keyUp(event), false)
 
